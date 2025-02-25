f4328639478ceb090106d93a629473b57d1f92ea
Hédi METTALI <hmettali@mobelite.fr>
2025-02-25 14:37:54 +0100

    v0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,169 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+        <main>
+            <div className={'px-24 pt-24 pb-12 p-6'}>
+                <h1 className={'text-5xl font-bold mb-2 text-violet-400'}>
+                    Hédi METTALI
+                </h1>
+                <h2 className={'text-2xl font-bold mb-2 text-gray-300'}>
+                    Frontend expect
+                </h2>
+                <p className={'text-gray-300'}>
+                    Crafting exceptional digital experiences with modern web technologies. Specialized in building
+                    scalable full-stack applications using MongoDB, Express.js, React, and Node.js.
+                </p>
+                <a>LinkedIN</a>
+                <a>Email</a>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <div>
+                <h2 className={'ml-24 mb-4 text-2xl'}>Professional Experience</h2>
+
+                <div className={'mx-24 my-12 p-6 bg-gray-50 text-stone-700 rounded-xl shadow-lg'}>
+                    <div>
+                        <h3>Freelance</h3>
+                        <div>Location</div>
+                        <div>2024 - Present</div>
+                        <h4>MERN Stack Developer</h4>
+                        <ul>
+                            <li>Developing custom web applications for international clients</li>
+                            <li>Building responsive and scalable frontend interfaces with React</li>
+                            <li>Implementing secure backend systems with Node.js and Express</li>
+                            <li> Collaborating with clients to deliver high-quality solutions</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={'mx-24 my-12 p-6 bg-gray-50 text-stone-700 rounded-xl'}>
+                    <div>
+                        <h3>Freelance</h3>
+                        <div>Location</div>
+                        <div>2024 - Present</div>
+                        <h4>MERN Stack Developer</h4>
+                        <ul>
+                            <li>Developing custom web applications for international clients</li>
+                            <li>Building responsive and scalable frontend interfaces with React</li>
+                            <li>Implementing secure backend systems with Node.js and Express</li>
+                            <li> Collaborating with clients to deliver high-quality solutions</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className={'px-24 pt-24 p-6 mb-12'}>
+                <h2 className={'mb-4 text-2xl'}>Skills & Expertise</h2>
+
+                <div className={'flex'}>
+                    <div className={'mr-4 p-6 bg-gray-50 text-stone-700 rounded-xl shadow-lg'}>
+                        <h3>Frontend Development</h3>
+                        <div>React.js, Next.js</div>
+                        <p>
+                            Building responsive and interactive user interfaces with modern React features and Next.js
+                            for optimal performance.
+                        </p>
+                    </div>
+                    <div className={'mr-4 p-6 bg-gray-50 text-stone-700 rounded-xl shadow-lg'}>
+                        <h3>UI/UX Design</h3>
+                        <div>Tailwind CSS, Material UI</div>
+                        <p>
+                            Crafting beautiful and intuitive user interfaces with modern design principles and
+                            frameworks.
+                        </p>
+                    </div>
+                    <div className={'mr-4 p-6 bg-gray-50 text-stone-700 rounded-xl shadow-lg'}>
+                        <h3>Version Control</h3>
+                        <div>Git, GitHub</div>
+                        <p>
+                            Managing code versions efficiently with Git and collaborating effectively through GitHub.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div>
+                <h2 className={'ml-24 text-2xl'}>Education & certification</h2>
+                <div className={'mx-24 my-12 p-6 bg-gray-50 text-stone-700 rounded-xl'}>
+                    <div>
+                        <h3>Bachelor's Degree in Computer Science</h3>
+                        <div>ISAMM</div>
+                        <div>2001 - 2004</div>
+                        <h4>MERN Stack Developer</h4>
+                        <ul>
+                            <li>Developing custom web applications for international clients</li>
+                            <li>Building responsive and scalable frontend interfaces with React</li>
+                            <li>Implementing secure backend systems with Node.js and Express</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={'mx-24 my-12 p-6 bg-gray-50 text-stone-700 rounded-xl'}>
+                    <div>
+                        <h3>Bachelor's Degree in Computer Science</h3>
+                        <div>ISAMM</div>
+                        <div>2001 - 2004</div>
+                        <h4>MERN Stack Developer</h4>
+                        <ul>
+                            <li>Developing custom web applications for international clients</li>
+                            <li>Building responsive and scalable frontend interfaces with React</li>
+                            <li>Implementing secure backend systems with Node.js and Express</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={'mx-24 my-12 p-6 bg-gray-50 text-stone-700 rounded-xl'}>
+                    <div>
+                        <h3>Bachelor's Degree in Computer Science</h3>
+                        <div>ISAMM</div>
+                        <div>2001 - 2004</div>
+                        <h4>MERN Stack Developer</h4>
+                        <ul>
+                            <li>Developing custom web applications for international clients</li>
+                            <li>Building responsive and scalable frontend interfaces with React</li>
+                            <li>Implementing secure backend systems with Node.js and Express</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+             <div className={'pl-24 pt-24 p-6'}>
+                    <h2 className={'mb-6 text-2xl'}>Contact</h2>
+
+                    <div className={'flex'}>
+                        <div className={'w-1/5 mr-4 p-6 bg-gray-50 text-stone-700 rounded-xl'}>
+                            <h3>Information de contact</h3>
+                            <div>hedi.mettali@gmail.com</div>
+                            <div>06 75 36 86 36</div>
+                            <div>Paris</div>
+                        </div>
+                        <div className={'w-4/5 mr-4 p-6 bg-gray-50 text-stone-700 rounded-xl'}>
+                            <form>
+                                <div className={'mb-6'}>
+                                    <label>Nom & Prénom</label>
+                                    <div>
+                                        <input className={"w-full h-10 border-2 rounded-lg border-stone-300"}/>
+                                    </div>
+                                </div>
+                                <div className={'mb-6'}>
+                                    <label>Sujet</label>
+                                    <div>
+                                        <input className={"w-full h-10 border-2 rounded-lg border-stone-300"}/>
+                                    </div>
+                                </div>
+                                <div className={'mb-6'}>
+                                    <label>Message</label>
+                                    <div>
+                                        <textarea className={"w-full h-48 border-2 rounded-lg border-stone-300"}>
+                                        </textarea>
+                                    </div>
+                                </div>
+                                <div>
+                                <button className={'w-full bg-blue-500 text-gray-50 p-2'} type="submit">Envoyer</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+             </div>
+
+        </main>
     </div>
   );
 }
